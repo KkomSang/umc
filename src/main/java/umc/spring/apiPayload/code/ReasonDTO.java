7dac7355273bc8edc22f2a98416c538549b64963
sangeun <olsang1203@gmail.com>
2024-06-05 22:44:01 +0900

    feat : API 응답 통일 및 에러 핸들러 세팅

---
+++ src/main/java/umc/spring/apiPayload/code/ReasonDTO.java
@@ -0,0 +1,17 @@
+package umc.spring.apiPayload.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReasonDTO {
+    private String code;
+    private String message;
+    private Boolean isSuccess;
+    private HttpStatus httpStatus;
+}
